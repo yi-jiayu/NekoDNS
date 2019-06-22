ddc165e918d81f7d1294bde83c86555d71d7bcd1
Jiayu Yi <yijiayu@gmail.com>
2019-06-22 14:29:39 +0800

    Implement adding domains

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -5,4 +5,10 @@ class DomainsController < ApplicationController
 
   def new
   end
+
+  def create
+    root = params.require(:root)
+    Domain.create(root: root, user: current_user)
+    redirect to domains_path
+  end
 end
