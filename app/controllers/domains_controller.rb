0c5b0e70440fb0443dd495a0ce7f41670d8af4a4
Jiayu Yi <yijiayu@gmail.com>
2019-06-24 21:15:57 +0800

    Create new domains in DomainsController

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -8,7 +8,7 @@ class DomainsController < ApplicationController
 
   def create
     root = params.require(:root)
-    domain = Domain.create(user: current_user, root: root)
+    domain = DomainService.create_domain(current_user, root)
     redirect_to domain
   end
 
