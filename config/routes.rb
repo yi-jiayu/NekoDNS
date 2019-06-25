f3b5b98e117921dd24b0655706dee4259a661bd1
Jiayu Yi <yijiayu@gmail.com>
2019-06-25 20:35:25 +0800

    Add Account controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get "login", to: "login#index", as: :login
-  get "auth/:provider/callback", to: "sessions#create", as: :oauth_callback
   get "logout", to: "sessions#destroy", as: :logout
+  get 'account', to: 'account#index', as: :account
+
+  get "auth/:provider/callback", to: "sessions#create", as: :oauth_callback
+  post "telegram/webhook", to: "telegram#webhook"
 
   resources :domains
 end
