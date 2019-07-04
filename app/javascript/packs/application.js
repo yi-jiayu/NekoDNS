2679d05293c36ee1147cefdd694e4b0e17140f14
Jiayu Yi <yijiayu@gmail.com>
2019-07-04 15:09:47 +0800

    Revert "rails: Stop loading unused frameworks"
    This reverts commit a0facf8c

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
+require("@rails/activestorage").start()
+require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
